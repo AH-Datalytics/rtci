ed406957012a80dd755090a729a64c7d079e162b
Oscar Boochever <obooch12@gmail.com>
2024-07-03 18:39:50 -0400

    working with external CSS

--- deploy/rtci/ui.R
+++ deploy/rtci/ui.R
@@ -2,64 +2,73 @@
 
 # UI Definition
 ui <- fluidPage(
+  # Include external CSS and JavaScript files
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
     tags$script(src = "index.js")
   ),
-  div(class = "center-div",  
-      h2(class = "title", "Real-Time Crime Index")
+  
+  # Title section
+  div(class = "center-div",  # Centered div for the title
+      h2(class = "title", "Real-Time Crime Index")  # Main title
   ),
-  div(class = "full-width-div",  
-      fluidRow(
-        column(12, div(class = "action-button",
-                       span("I want to see "),
-                       div(class = "select-wrapper", 
-                           selectInput("crimeType", "", choices = unique(data$`Crime Type`), multiple = TRUE, selected = default_crime_type, width = '100%')
-                       ),
-                       span(" for "),
-                       div(class = "select-wrapper", 
-                           selectInput("agencyName", "", choices = unique(data$`Agency Name`), selected = default_agency, width = '100%')
-                       ),
-                       span(" from "),
-                       div(class = "select-wrapper", 
-                           selectInput("yearFilter", "", choices = unique(data$Year), multiple = TRUE, selected = default_years, width = '100%')
-                       )
-        ))
+  
+  # Filter section
+  div(class = "full-width-div",  # Full width div to contain the filter elements
+      # Flexbox container to arrange the elements horizontally and center them
+      span("I want to see "),  # Text for filter sentence
+      div(class = "select-wrapper",  # Wrapper div for the crime type select input
+          selectInput("crimeType", "", choices = unique(data$`Crime Type`), multiple = TRUE, selected = default_crime_type, width = '100%')
       ),
-      fluidRow(
-        column(12, div(class = "action-button",
-                       actionButton("toggleView", "View Table", class = "btn-primary")
-        ))
+      span(" for "),  # Text for filter sentence
+      div(class = "select-wrapper",  # Wrapper div for the agency name select input
+          selectInput("agencyName", "", choices = unique(data$`Agency Name`), selected = default_agency, width = '100%')
+      ),
+      span(" from "),  # Text for filter sentence
+      div(class = "select-wrapper",  # Wrapper div for the year filter select input
+          selectInput("yearFilter", "", choices = unique(data$Year), multiple = TRUE, selected = default_years, width = '100%')
       )
   ),
+  
+  # Button section
+  div(class = "filter-button-container",  # Separate container for the View Table button
+      actionButton("toggleView", "View Table", class = "btn-primary")  # View Table button
+  ),
+  
+  # Main panel section
   mainPanel(
-    class = "main-panel",
-    uiOutput("viewOutput"),
+    class = "main-panel",  # Main panel container
+    uiOutput("viewOutput"),  # Dynamic UI output for either the plot or the data table
+    
+    # KPI boxes section
     fluidRow(
       column(4,
-             wellPanel(
+             wellPanel(  # Well panel for Current Year-to-Date KPI
                h4("Current Year-to-Date"),
                h5(textOutput("currentYTD")),
                textOutput("currentYTDLabel")
              )
       ),
       column(4,
-             wellPanel(
+             wellPanel(  # Well panel for Previous Year-to-Date KPI
                h4("Previous Year-to-Date"),
                h5(textOutput("previousYTD")),
                textOutput("previousYTDLabel")
              )
       ),
       column(4,
-             wellPanel(
+             wellPanel(  # Well panel for Percent Change YTD KPI
                h4("Percent Change YTD"),
                h5(textOutput("percentChangeYTD")),
                textOutput("percentChangeYTDLabel")
              )
       )
     ),
-    div(class = "text-right",
-        downloadButton("downloadData", "Download Table"),
-        downloadButton("downloadPlot", "Download Graph"))
+    
+    # Download buttons section
+    div(class = "text-right",  # Right-aligned container for download buttons
+        downloadButton("downloadData", "Download Table"),  # Download Table button
+        downloadButton("downloadPlot", "Download Graph")  # Download Graph button
+    )
   )
 )

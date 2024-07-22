1e3740dc85ea300bb5b05fdeceed5575b67eaa07
Oscar Boochever <obooch12@gmail.com>
2024-07-22 17:22:41 -0400

    viz ready script fixes

--- scripts/final_sample_to_viz.R
+++ scripts/final_sample_to_viz.R
@@ -8,6 +8,13 @@ final_sample <- read_csv("data/sample_2018_present_wlinks.csv")
 final_sample <- final_sample %>%
     rename_with(~ str_replace_all(tolower(.), "\\.", "_"))
 
+
+# Custom function to capitalize first letter of each word and keep "PD" capitalized
+capitalize_words <- function(x) {
+  str_to_title(x) %>%
+    str_replace_all("\\bPd\\b", "PD")
+}
+
 # Capitalize first letter of character columns, rest lowercase
 final_sample <- final_sample %>%
   mutate(across(where(is.character), ~ str_to_title(.))) %>%
@@ -16,7 +23,8 @@ final_sample <- final_sample %>%
                   "state", 
                   "state_name", 
                   "ucr_agency_name"), 
-                ~ str_to_sentence(.)))
+                ~ str_to_sentence(.))) %>%
+  mutate(across(c("agency_name", "city"), ~ capitalize_words(.)))
 
 # Create 'agency_full' and 'location_full' columns
 final_sample <- final_sample %>%
@@ -59,6 +67,10 @@ final_sample_long <- final_sample %>%
                names_to = "crime_type",
                values_to = "count")
 
+# Capitalize the first letter of each word in the crime type and handle MVT
+final_sample_long <- final_sample_long %>%
+  mutate(crime_type = str_replace_all(str_to_title(crime_type), "Mvt", "MVT"))
+
 # Final arrangement of columns
 final_sample_long <- final_sample_long %>%
   select(date, 
@@ -76,3 +88,5 @@ final_sample_long <- final_sample_long %>%
 # Print the first few rows of the cleaned data
 head(final_sample_long)
 
+# Write the final_sample_long data frame to viz_data.csv
+write.csv(final_sample_long, "custom_web_app/data/viz_data.csv", row.names = FALSE)
